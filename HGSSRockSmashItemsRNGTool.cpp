32a851b908986b8c4f518746b462dab8ab7f9643
Real96 <chicco.gaeta@gmail.com>
2026-05-06 22:36:49 +0200

    Add lead ability support

--- HGSSRockSmashItemsRNGTool.cpp
+++ HGSSRockSmashItemsRNGTool.cpp
@@ -25,10 +25,19 @@ enum Location {
     RUINS_OF_ALPH = 15
 };
 
+enum Ability {
+    NONE = 1,
+    SUCTION_CUPS = 2,
+    MAGNET_PULL = 3,
+    KEEN_EYE = 4,
+    SERENE_GRACE = 5,
+    SUPER_LUCK = 6
+};
+
 void printLocations(array<string_view, 15> names) {
     cout << "Locations:\n\n";
 
-    for (size_t i = 0; i < names.size(); i++) {
+    for (size_t i = 0; i < 15; i++) {
         cout << i + 1 << " " << names[i] << "\n";
     }
 
@@ -82,18 +91,23 @@ void getItemInput(const short location, const string locationName, short &item) 
     cout << locationName << " items:\n\n";
     printItemsName(location);
     sanitizeInput<short>("Insert the wanted item number: ", item, 1, location == CLIFF_CAVE ? 7 : 8);
+    cout << "\n\n";
 }
 
-void sanitizeHexInput(uint32_t &seed) {
-    string stringSeed;
-    const regex hexRegex("^[0-9a-fA-F]{1,8}$");
+void printLeadAbilities() {
+    static constexpr array<string_view, 6> abilityNames{ "None", "Suction Cups", "Magnet Pull", "Keen Eye", "Serene Grace", "Super Luck" };
+    cout << "Abilities:\n\n";
 
-    while ((cout << "Insert the initial seed: ") && (!(cin >> stringSeed) || !regex_match(stringSeed, hexRegex))) {
-        cin.clear();
-        cin.ignore(numeric_limits<streamsize>::max(), '\n');
+    for (size_t i = 0; i < 6; i++) {
+        cout << i + 1 << " " << abilityNames[i] << "\n";
     }
 
-    seed = stoul(stringSeed, nullptr, 16);
+    cout << "\n\n";
+}
+
+void getAbilityInput(short &ability) {
+    printLeadAbilities();
+    sanitizeInput<short>("Insert  number of the ability of your party leader pokemon: ", ability, 1, 6);
 }
 
 bool sanitizeYesNoInput(const string output) {
@@ -106,6 +120,51 @@ bool sanitizeYesNoInput(const string output) {
     }
 
     return toupper(yesNoAnswer[0]) == 'Y';
+}
+
+void getLeadRockSmashInput(bool &leadUsingRockSmash) {
+    leadUsingRockSmash = sanitizeYesNoInput("Is your party leader pokemon using RockSmash HM? (y/n) ");
+}
+
+void setThresholdIncreaser(short &thresholdIncreaser, const short ability, const bool leadUsingRockSmash) {
+    switch (ability) {
+    case SUCTION_CUPS:
+    case MAGNET_PULL:
+    case KEEN_EYE:
+        thresholdIncreaser = 5;
+        break;
+    default:
+        thresholdIncreaser = 0;
+        break;
+    }
+
+    if (leadUsingRockSmash) {
+        thresholdIncreaser += 5;
+    }
+}
+
+void setItemIndexIncreaser(short &itemIncreaser, const short location, const short item, short const ability) {
+    switch(ability) {
+        case SERENE_GRACE:
+        case SUPER_LUCK:
+            itemIncreaser = 1;
+            break;
+        default:
+            itemIncreaser = 0;
+            break;
+    }
+}
+
+void sanitizeHexInput(uint32_t &seed) {
+    string stringSeed;
+    const regex hexRegex("^[0-9a-fA-F]{1,8}$");
+
+    while ((cout << "Insert the initial seed: ") && (!(cin >> stringSeed) || !regex_match(stringSeed, hexRegex))) {
+        cin.clear();
+        cin.ignore(numeric_limits<streamsize>::max(), '\n');
+    }
+
+    seed = stoul(stringSeed, nullptr, 16);
 }
 
 void getRNGInput(uint32_t &seed, unsigned long &advances, bool &knownSeedFlag) {
@@ -152,22 +211,46 @@ bool encounterCheck(uint32_t &seed, const short location) {
     return getHighSeed(advanceRNG(seed)) % 100 < encounterRateThresholds[location - 1];
 }
 
-bool itemCheck(uint32_t &seed, const short location) {
+bool itemCheck(uint32_t &seed, const short location, const short thresholdIncreaser) {
     static constexpr array<short, 15> itemRateThresholds{ 25, 50, 15, 10, 25, 25, 25, 30, 20, 50, 30, 20, 25, 10, 30 };
 
-    return getHighSeed(advanceRNG(seed)) % 100 < itemRateThresholds[location - 1];
+    return getHighSeed(advanceRNG(seed)) % 100 < itemRateThresholds[location - 1] + thresholdIncreaser;
 }
 
-bool isWantedItemCheck(uint32_t seed, const short locationThresholds, const short item) {
-    static constexpr array<short, 8> itemThresholds0{ 0, 25, 45, 55, 65, 75, 95, 100 };
-    static constexpr array<short, 9> itemThresholds1{ 0, 25, 45, 55, 65, 75, 85, 95, 100 };
+short getGeneratedItem(const short locationThresholds, uint32_t seed, short itemIncreaser) {
+    static constexpr array itemThresholds0{ to_array<short>({ 25, 45, 55, 65, 75, 95, 100 }) };
+    static constexpr array itemThresholds1{ to_array<short>({ 25, 45, 55, 65, 75, 85, 95, 100 }) };
 
-    static constexpr array<const short*, 2> itemThresholds{ itemThresholds0.data() , itemThresholds1.data() };
+    using strview_span = span<const short>;
 
-    return getHighSeed(advanceRNG(seed)) % 100 >= itemThresholds[locationThresholds][item - 1] && getHighSeed(seed) % 100 < itemThresholds[locationThresholds][item];
+    static constexpr array itemThresholds{ to_array<strview_span>({
+        span<const short>( itemThresholds0 ),
+        span<const short>( itemThresholds1 )
+    }) };
+
+    strview_span itemGroupTresholds = itemThresholds[locationThresholds];
+
+    for (short item = 0; item < itemGroupTresholds.size(); item++) {
+        uint16_t highSeed = getHighSeed(seed) % 100;
+
+        if (highSeed < itemGroupTresholds[item]) {
+            if (locationThresholds == 0 && item == 5 && highSeed < 85) {
+                itemIncreaser = 0;
+            }
+            else if (itemIncreaser) {
+                itemIncreaser = locationThresholds == 0 ? (item + 1 == 7 ? 0 : 1) : (item + 1 == 8 ? 0 : 1);
+            }
+
+            return (item + 1) + itemIncreaser;
+        }
+    }
 }
 
-void findItem(const short location, uint32_t seed, unsigned long advances, const short item) {
+bool isWantedItemCheck(uint32_t seed, const short locationThresholds, const short item, short itemIncreaser) {
+    return getGeneratedItem(locationThresholds, advanceRNG(seed), itemIncreaser) == item;
+}
+
+void findItem(const short location, uint32_t seed, unsigned long advances, const short item, const short thresholdIncreaser, const short itemIndexIncreaser) {
     for (;; advances++, advanceRNG(seed)) {
         uint32_t tempSeed = seed;
 
@@ -177,24 +260,29 @@ void findItem(const short location, uint32_t seed, unsigned long advances, const
             }
         }
 
-        if (!itemCheck(tempSeed, location)) {
+        if (!itemCheck(tempSeed, location, thresholdIncreaser)) {
             continue;
         }
 
-        if (isWantedItemCheck(tempSeed, location == CLIFF_CAVE ? 0 : 1, item)) {
-            printf("\n\nTarget seed: %08X | Target advances: %lu\n\n------------------------------------------------\n\n", seed, advances);
+        if (item == 1 && itemIndexIncreaser) {
+            printf("\n\nYou cannot get the slot 1 item with ability Serene Grace / Super Luck");
+            return;
+        }
+
+        if (isWantedItemCheck(tempSeed, location == CLIFF_CAVE ? 0 : 1, item, itemIndexIncreaser)) {
+            printf("\n\nTarget seed: %08X | Target advances: %lu", seed, advances);
             return;
         }
     }
 }
 
-void findItemSeed(const short location, const short item) {
+void findItemSeed(const short location, const short item, const short thresholdIncreaser, const short itemIndexIncreaser) {
     const short hour = 24, maxDelay = 10000;
     short minDelay;
     unsigned long maxAdvances;
 
     sanitizeInput<short>("Insert the min delay: ", minDelay, 600, 9999);
-    sanitizeInput<unsigned long>("Insert the max advances: ", maxAdvances, 1, ULONG_MAX);
+    sanitizeInput<unsigned long>("Insert the max advances: ", maxAdvances, 0, ULONG_MAX);
 
     for (short ab = 0; ab < 256; ab++) {
         for (short cd = 0; cd < hour; cd++) {
@@ -202,7 +290,7 @@ void findItemSeed(const short location, const short item) {
                 const uint32_t seed = ((ab << 24) | (cd << 16)) + efgh;
                 uint32_t tempSeed = seed;
 
-                for (unsigned long advances = 0; advances < maxAdvances; advances++, advanceRNG(tempSeed)) {
+                for (unsigned long advances = 0; advances <= maxAdvances; advances++, advanceRNG(tempSeed)) {
                     uint32_t tempSeed2 = tempSeed;
 
                     if (getWildCheckFlag(location)) {
@@ -211,12 +299,17 @@ void findItemSeed(const short location, const short item) {
                         }
                     }
 
-                    if (!itemCheck(tempSeed2, location)) {
+                    if (!itemCheck(tempSeed2, location, thresholdIncreaser)) {
                         continue;
                     }
 
-                    if (isWantedItemCheck(tempSeed2, location == CLIFF_CAVE ? 0 : 1, item)) {
-                        printf("\n\nTarget seed: %08X | Target advances: %lu\n\n------------------------------------------------\n\n", seed, advances);
+                    if (item == 1 && itemIndexIncreaser) {
+                        printf("\n\nYou cannot get the slot 1 item with ability Serene Grace / Super Luck");
+                        return;
+                    }
+
+                    if (isWantedItemCheck(tempSeed2, location == CLIFF_CAVE ? 0 : 1, item, itemIndexIncreaser)) {
+                        printf("\n\nTarget seed: %08X | Target advances: %lu", seed, advances);
                         return;
                     }
                 }
@@ -226,23 +319,29 @@ void findItemSeed(const short location, const short item) {
 }
 
 int main() {
-    short locationIndex, itemIndex;
+    short locationIndex, itemIndex, ability, thresholdIncreaser, itemIndexIncreaser;
     string locationName;
     uint32_t initialSeed;
     unsigned long currentAdvances;
-    bool knownSeedFlag;
+    bool leadUsingRockSmash, knownSeedFlag;
 
     while (true) {
         getLocationInput(locationIndex, locationName);
         getItemInput(locationIndex, locationName, itemIndex);
+        getAbilityInput(ability);
+        getLeadRockSmashInput(leadUsingRockSmash);
+        setThresholdIncreaser(thresholdIncreaser, ability, leadUsingRockSmash);
+        setItemIndexIncreaser(itemIndexIncreaser, locationIndex, itemIndex, ability);
         getRNGInput(initialSeed, currentAdvances, knownSeedFlag);
 
         if (knownSeedFlag) {
             advanceRNG(initialSeed, currentAdvances);
-            findItem(locationIndex, initialSeed, currentAdvances, itemIndex);
+            findItem(locationIndex, initialSeed, currentAdvances, itemIndex, thresholdIncreaser, itemIndexIncreaser);
+            printf("\n\n------------------------------------------------\n\n");
             continue;
         }
 
-        findItemSeed(locationIndex, itemIndex);
+        findItemSeed(locationIndex, itemIndex, thresholdIncreaser, itemIndexIncreaser);
+        printf("\n\n------------------------------------------------\n\n");
     }
 }
